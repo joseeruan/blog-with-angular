aa5e5ee7b3be597f839060d83e527b4d9c513c89
José Ruan <jrpaivadev@gmail.com>
2025-03-03 22:33:00 -0300

    feat: add 4 new components - big-card, menu-bar, menu-title, small-card

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  imports: [],
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.css',
+             './menu-title.responsive.component.css'
+  ]
+})
+
+export class MenuTitleComponent {
+
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-small-card',
+  imports: [RouterModule],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  id:string = "0"
+
+  constructor() {}    
+
+  ngOnInit(): void {
+      
+  }
+}
