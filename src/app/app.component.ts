b220928b2ddf3ef39a685e5d7a0d9263bc79397d
José Ruan <jrpaivadev@gmail.com>
2025-03-03 22:36:21 -0300

    chore(routes): update app routes and imports style(app): modify CSS for layout adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'FuteBlog';
 }
