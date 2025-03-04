d7d4424fdea25c786cf2028b78697b47ee3d3add
José Ruan <jrpaivadev@gmail.com>
2025-03-03 22:37:06 -0300

    chore(data): add fake data for testing purposes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Messi ganha a copa do mundo",
+    "description": "O jogador venceu nesse último domingo a sua primeira copa do mundo.",
+    "photoCover": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/02/messi-inter-miami-sporting-kansas-city-e1740060094278.jpg?w=1080"
+  },
+  {
+    "id": "2",
+    "title": "Mbappé se destaca na La Liga",
+    "description": "Kylian Mbappé brilhou novamente no campeonato espanhol, marcando mais um gol na vitória do Real Madrid.",
+    "photoCover": "https://media.gettyimages.com/id/2200688017/pt/foto/madrid-spain-kylian-mbappe-of-real-madrid-celebrates-scoring-his-teams-third-goal-and-his-hat.jpg?s=612x612&w=0&k=20&c=7Hbh5lH79t6UajzVrnNVy2b06uVbkKkhMFGab0sXOX0="
+  },
+  {
+    "id": "3",
+    "title": "Endrick decide para o Real Madrid",
+    "description": "Endrick, o jovem prodígio, foi decisivo com seu gol na vitória contra o Barcelona, garantindo três pontos importantes para o Real.",
+    "photoCover": "https://media.gettyimages.com/id/2168423940/pt/foto/madrid-spain-endrick-of-real-madrid-celebrates-scoring-his-teams-third-goal-during-the-la-liga.jpg?s=612x612&w=0&k=20&c=gMdgILWiJmFOg1QaGEa-Q2Ulyb1yYkSDg3ysw2WLTMo="
+  },
+  {
+    "id": "4",
+    "title": "Neymar supera marca histórica no Santos",
+    "description": "Neymar, ídolo santista, quebra mais um recorde com uma nova marca histórica de gols no clube.",
+    "photoCover": "https://media.gettyimages.com/id/2200710217/pt/foto/santos-brazil-neymar-of-santos-celebrates-the-second-goal-of-his-team-scored-by-tiquinho.jpg?s=612x612&w=0&k=20&c=Rbqs-6sGI0sAB6HyOm6YHKQ3DBbU9LWaIw4kztwC-qk="
+  },
+  {
+    "id": "5",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Disney+ acaba de anunciar uma nova série exclusiva para os fãs de Marvel e Star Wars.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+];
